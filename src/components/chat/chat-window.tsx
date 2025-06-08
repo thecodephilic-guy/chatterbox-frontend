3946b4a45c15ea21b47f430bd9b5e52c790d7aec
Sohail Ansari <sohailansari483@gmail.com>
2025-06-08 20:41:30 +0530

    defined chat area

--- src/components/chat/chat-window.tsx
+++ src/components/chat/chat-window.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ChatHeader from "./chat-header";
 import { useChatStore } from "@/store/chat-store";
+import ChatInput from "./chat-input";
 
 function ChatWindow() {
   const selectedChat = useChatStore((state) => state.selectedChat);
@@ -9,13 +10,31 @@ function ChatWindow() {
   return (
     <>
       {selectedChat ? (
-        <ChatHeader />
-      ) : !isChatLoading && (
-        <div className="flex h-full items-center justify-center">
-          <h1 className="text-2xl text-gray-500 font-medium text-center m-0">
-            Select a chat to start the conversation
-          </h1>
-        </div>
+        <>
+          <div className="flex flex-col h-screen">
+            <div className="shrink-0">
+              <ChatHeader />
+            </div>
+            <div className="flex-1 overflow-y-auto w-full px-4 py-6" id="chat-area">
+              {/* Chat messages will be rendered here */}
+              <div className="text-gray-300 text-center">
+                This is the chat area where complete chat will be displayed
+              </div>
+              {/* Implement infinite scroll logic here */}
+            </div>
+            <div className="shrink-0 border-t px-4 py-3">
+              <ChatInput />
+            </div>
+          </div>
+        </>
+      ) : (
+        !isChatLoading && (
+          <div className="flex h-full items-center justify-center">
+            <h1 className="text-2xl text-gray-500 font-medium text-center m-0">
+              Select a chat to start the conversation
+            </h1>
+          </div>
+        )
       )}
     </>
   );
