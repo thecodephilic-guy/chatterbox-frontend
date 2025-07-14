d058927a9b164b605e78d8651a11694437ed7ddb
Sohail Ansari <sohailansari483@gmail.com>
2025-07-14 14:35:39 +0530

    fixed deployment bug

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.SERVER_ENDPOINT || "http://localhost:8080/api",
+  baseURL: process.env.SERVER_ENDPOINT,
   withCredentials: true
 });
 
